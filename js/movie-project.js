dea6609691b40c60fc1e93160ffba6407bec97e2
Wyatt DeRamus <deramus.wyatt@gmail.com>
2022-12-14 15:30:42 -0600

    moving to the deleting portion of the movies

--- js/movie-project.js
+++ js/movie-project.js
@@ -5,14 +5,10 @@
 //     alert("Loading page");
 // })
 
-//Setting glitch url to variable URL
-// const URL = `https://ossified-wiggly-tarantula.glitch.me/movies`;
-
-
-
 console.log("setTimeout() example...");
 let cardContainer = $("#card");
 let loading = $("#card").html();
+let selectedid;
 const renderMovies = () => {
     fetch("https://ossified-wiggly-tarantula.glitch.me/movies")
         .then(resp => resp.json())
@@ -27,7 +23,7 @@ const renderMovies = () => {
                 let id = data[i].id;
 
                 html +=
-                    '<div class="card" style="width: 18rem;">' +
+                    `'<div class="card clickMe" id='${id}' style="width: 18rem;">'` +
 
                             '<div class="card-body">' +
                                 '<h5 class="card-title" id="title">' + title + '</h5>' +
@@ -49,81 +45,44 @@ const renderMovies = () => {
                 $("#edit").val($(this).parent().parent().children().first().children().first().html());
                 $("#editRate").val($(this).parent().parent().children().first().next().children().last().html());
                 $("#editId").val($(this).parent().parent().children().first().next().children().first().html());
-
             });
+            $(`.clickMe`).click(function() {
+                selectedid = $(this).attr('id');
+                console.log(selectedid);
+            })
         });
 }
 
-
-
+// Takes the movie ID from an edit-button and populates the edit form with the current data for that movie
 const input2 = document.getElementById("edit");
 const rate2 = document.getElementById("editRate");
-const btn2 = document.getElementById("submit2");
-btn2.addEventListener("click", () => {
+
+// letting the submit button know what data to grab
+$(`#submit2`).click(function(){
+    populateEdit();
+    console.log("fired")
+})
+//Editing the movie function
+function populateEdit() {
+    // First, get the information for that movie ID
     const edittedMovie = {
+        id: selectedid,
         title: input2.value,
         rating: rate2.value
     }
-    console.log(newMovie);
-    fetch("https://ossified-wiggly-tarantula.glitch.me/movies",{
-        method: 'PUT',
+    fetch("https://ossified-wiggly-tarantula.glitch.me/movies/" + selectedid,{
+
+        method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(edittedMovie),
     })
-        .then(resp => resp.json())
-        .then(data => {
-
-            renderMovies(data)
-        });
-});
-// FORM SUBMISSION: EDIT FUNCTION
-// On click of "Apply" Button, will create an object with the inputs on the edit form and send to the editMovie() function
-// $('#submit2').click(function (e) {
-//     e.preventDefault();
-//     let title = $('#title').val().html;
-//     let rating = $('#rating').val();
-//     let id = $('#id').val();
-//     let editedMovie = {id, title, rating};
-//     console.log(editedMovie);
-//     editMovie();
-// });
-
-// PATCH REQUEST
-// Takes the movie from the Edit Form and uses PATCH to change the values of it's attributes in the JSON file
-// const editMovie = (movie) => {
-//     let edittedMovie = {
-//         method: 'PUT',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify(movie)
-//     }
-//     return fetch("https://ossified-wiggly-tarantula.glitch.me/movies", options)
-//         .then(resp => resp.json())
-//         .then(renderMovies)
-//         .catch(err => console.error(err));
-// }
-//
-// $('#submit2').click(function (e) {
-//               var index = $(this).data.id
-//
-//              console.log(index)
-//             e.preventDefault()
-//             let editTitle = $('#title').val
-//             let editRating = $('#rating').val(data[index].rating)
-//
-//             console.log(editTitle)
-//             // let editMovieObj = {
-//             //     title: editTitle ,
-//             //     rating: editRating,
-//             //
-//             // }
-//             //  return editMovie(editMovieObj)
-//         })
-
+        // Take the movie attributes and set the values within the edit form to match the value of each attribute
+        .then(()=> renderMovies())
+}
 
 renderMovies();
-
 
 //set variables for the project
 const input = document.getElementById("addNew");
@@ -149,58 +108,6 @@ btn.addEventListener("click", () => {
         });
         });
 
-// function editMe(id){
-//     console.log($('#title').val());
-//     fetch("https://ossified-wiggly-tarantula.glitch.me/movies/" + id, {
-//         method: "PUT",
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify(
-//             {
-//                 title:$('#title').val(),
-//                 rating: $("#rating").val()
-//             }
-//         )
-//     })
-//         .then(response => response.json())
-//         .then(json => {
-//             $('.card-title').innerText = `${json.title}`
-//             $('.card-text').innerText = `${json.overview}`
-//         })
-// }
-
-
-
-//Edit click
-// let editTitle = $(".card-title").html()
-// let editRate = $(".list-group-item").html
-// $("#edit").click(function (){
-//     $(this).value().data()
-// })
-
-
-
-// const messageArea2 = document.getElementById("show2");
-// const edit = document.getElementById("edit");
-// const editRate = document.getElementById("editRate");
-// const btn2 = document.getElementById("submit2");
-// btn2.addEventListener("click", () => {
-//     const editMovie = {
-//         title: edit.value,
-//         rating: editRate.value
-//     }
-//     fetch("https://ossified-wiggly-tarantula.glitch.me/movies",{
-//         method: 'PATCH',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(editMovie),
-//     })
-//         .then(resp => resp.json())
-//         .then(data => {
-//             console.log(data);
-//             messageArea2.innerText = "Request Successful! Check console for your information.";
-//         });
-// });
 
 
 
@@ -209,40 +116,6 @@ btn.addEventListener("click", () => {
 
 
 
-
-
-
-
-
-
-// function edittedMovies(movies) {
-//     let options = {
-//         method: "PATCH",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(movies)
-//     }).then(() => fetch("https://ossified-wiggly-tarantula.glitch.me/movies"))
-//         .then(resp => resp.json())
-//         .then(movies => console.log(movies));
-//     let num = parseInt(movies.id)
-//     return fetch("https://ossified-wiggly-tarantula.glitch.me/movies" + "/" + num, options)
-// }
-
-//
-// function editMovie(movies) {
-//     let options = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(movies)
-//     }
-//     // let num = parseInt(movies.id)
-//     return fetch("https://ossified-wiggly-tarantula.glitch.me/movies" + "/" + options)
-//         .then((callJson) => callJson.json())
-// }
-// console.log(editMovie())
 
 
 
