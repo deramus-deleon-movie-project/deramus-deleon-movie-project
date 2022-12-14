68a7ec03b5ca2677af9ab6d39823c454e91d5f84
Crystal <crystal.m.deleon00@gmail.com>
2022-12-14 16:51:01 -0600

    done

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,10 +1,3 @@
-
-// Loading page
-// $(window).on('load', function () {
-//     $('#loading').hide();
-//     alert("Loading page");
-// })
-
 console.log("setTimeout() example...");
 let cardContainer = $("#card");
 let loading = $("#card").html();
@@ -34,7 +27,7 @@ const renderMovies = () => {
                                 '</ul>' +
                             '<div class="card-body">' +
                                 '<a href="#" class="card-link edit" >Edit</a>' +
-                                '<a href="#" class="card-link" id="delete">Delete</a>' +
+                                '<a href="#" class="card-link delete">Delete</a>' +
                             '</div>' +
                     '</div>'
 
@@ -45,6 +38,23 @@ const renderMovies = () => {
                 $("#edit").val($(this).parent().parent().children().first().children().first().html());
                 $("#editRate").val($(this).parent().parent().children().first().next().children().last().html());
                 $("#editId").val($(this).parent().parent().children().first().next().children().first().html());
+            });
+            $(`.delete`).click(function(e) {
+                e.preventDefault()
+                let options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+
+                selectedid = $(this).parent().parent().children().first().next().children().first().html()
+                console.log(selectedid);
+
+                return fetch("https://ossified-wiggly-tarantula.glitch.me/movies/" + selectedid, options)
+                    .then(renderMovies)
+                    .catch(err => console.error(err));
+
             });
             $(`.clickMe`).click(function() {
                 selectedid = $(this).attr('id');
@@ -109,218 +119,22 @@ btn.addEventListener("click", () => {
         });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Loading page
-// $(window).on('load', function () {
-//     $('#loading').hide();
-// })
-//
-//
-//
-// // Loading page
-// //     $(window).on('load', function () {
-// //         $('#loading').hide();
-// //     })
-//
-//
-// const moviesAPI = 'https://ossified-wiggly-tarantula.glitch.me/movies';
-// let moviesClone = []
-//
-//
-// fetch(moviesAPI)
-//     .then((response) => response.json())
-//     .then((jsonData) => console.log(jsonData))
-//
-//
-// //Get all
-// function getMovies() {
-//     return fetch(moviesAPI).then((callJson) => callJson.json());
-// }
-//
-//
-// //Display data
-//
-// fetch(moviesAPI)
-//     .then((callForJson) => {
-//         return callForJson.json();
-//     }).then((movies) => {
-//     moviesClone = movies
-//     console.log(movies[0])
-//     for (var i = 0; i <= 13; i++) {
-//         $("#movie").append(
-//             "<div class='card col-lg-3 m-2 text-center'><h2 class='card-title'>" + movies[i].title + "</h2>"
-//             + "<img class='img-fluid'  src='" + movies[i].poster + "'>"
-//             + "<p>" + "Rating: " + movies[i].rating + "</p>"
-//             + "<p>" + "Genre: " + movies[i].genre + "</p>"
-//             + "<button class='delete'  data-id='" + movies[i].id + "'>" + "Delete Movie" + "</button>"
-//             + "<button class='edit' data-index='" + i + "' type='button' class='btn' data-toggle='modal'>" + "Edit Movie" + "</button></div>")
-//         // console.log($('.edit'))
-//         $('.edit').click(function (e) {
-//             var index = $(this).data('index')
-//             console.log(index)
-//             e.preventDefault()
-//             let editTitle = $('#newTitle').val(movies[index].title)
-//             let editGenre = $('#newGenre').val(movies[index].genre)
-//             let editRating = $('#newRating').val(movies[index].rating)
-//
-//             console.log(editTitle)
-//             let editMovieObj = {
-//                 title: editTitle ,
-//                 rating: editGenre,
-//                 genre: editRating
-//             }
-//             // return editMovie(editMovieObj)
-//         })
-//
-//         $(".delete").click(function(){
-//             var id = $(this).data("id")
-//             console.log(id)
-//             deleteMovie(id)
-//         })
-//     }
-//
-// });
-// // $('.delete').on("click", function() {console.log(this)
-// //     var id = $(this).data('id')
-// //     console.log(id)
-// //     deleteMovie(id)
-// // });
-// //      $(".delete").click(function(){
-// //         var id = thisOne.getAttribute("data-id")
-// //         console.log(id)
-// //         deleteMovie(id)
-// //     })
-// // console.log($('.edit'))
-// //     $('.edit').click(function () {
-// //         var index = $(this).data('index')
-// //         console.log(index)
-// //         e.preventDefault()
-// //         // let editTitle = $('#newTitle').val()
-// //         // let editGenre = $('#newGenre').val(movies[index].genre)
-// //         // let editRating = $('#newRating').val(movies[index].rating)
-// //         // editTitle.val(movies[i].title)
-// //         // let editMovieObj = {
-// //         //     title: editTitle ,
-// //         //     rating: editGenre,
-// //         //     genre: editRating
-// //         // }
-// //         // return editMovie(editMovieObj)
-// //     })
-//
-//
-// //Get a by ID
-// function getMovie(id) {
-//     return fetch('${moviesAPI/id}')
-//         .then((callJson) => callJson.json());
-// }
-//
-//
-// //function(11)
-//
-// function editMovie(movies) {
-//     let options = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(movies)
-//     }
-//     let num = parseInt(movies.id)
-//     return fetch(`${moviesAPI}` + "/" + num , options)
-//         .then((callJson) => callJson.json())
-// }
-//
-// // Delete by
-// function deleteMovie(id) {
+// $(`.delete`).click(function(e) {
+//     e.preventDefault()
 //     let options = {
 //         method: 'DELETE',
 //         headers: {
 //             'Content-Type': 'application/json',
 //         },
 //     }
-//     fetch(`${moviesAPI}/${id}`, options)
-//         .then((callJson) => console.log("Delete movie" + id, callJson))
-// }
 //
-// //
+//         selectedid = $(this).attr('id');
+//         console.log(selectedid);
 //
+//     return fetch("https://ossified-wiggly-tarantula.glitch.me/movies/" + selectedid, options)
+//         .then(renderMovies)
+//         .catch(err => console.error(err));
 //
-// // Create Movie
-// function addMovie(movie) {
-//     let options = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(movie)
-//     }
-//     return fetch(`${moviesAPI}`, options)
-//         .then((callJson) => callJson.json())
-//         .then((parsedData) => console.log(parsedData))
-//
-// }
-//
-// $('#submitNewMovie').click(function (e) {
-//     e.preventDefault()
-//     let newTitle = $('#newTitle').val()
-//     let newGenre = $('#newGenre').val()
-//     let newRating = $('#newRating').val()
-//
-//     let newMovieObj = {
-//         title: newTitle,
-//         rating: newRating,
-//         genre: newGenre
-//     }
-//     return addMovie(newMovieObj)
-// })
+// });
 
+console.log(deleteMovie);
